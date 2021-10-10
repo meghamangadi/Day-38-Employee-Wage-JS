9f01e911a8faf7a3ae6d72841e49eb0be8548bef
megha angadi <meghamangadi@gmail.com>
2021-10-10 11:47:36 +0530

    Added: Program to calculate wages for month

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -5,13 +5,7 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HRS = 4;
 const FULL_TIME_HRS = 8;
 const WAGE_PER_HRS = 20;
-
-let empHrs = 0;
-let empcheck = Math.floor(Math.random() * 10) % 3;
-getWorkingHours(empcheck);
-let empWage = empHrs * WAGE_PER_HRS;
-console.log("Daily Employee Wage :" + empWage);
-
+const NUM_OF_WORKING_DAYS = 20;
 
 function getWorkingHours(empcheck) {
     switch (empcheck) {
@@ -26,6 +20,16 @@ function getWorkingHours(empcheck) {
 
     }
 }
+let totalEmpHrs = 0;
+for (let day = 1; day <= NUM_OF_WORKING_DAYS; day++) {
+    let empcheck = Math.floor(Math.random() * 10) % 3;
+    getWorkingHours(empcheck);
+    totalEmpHrs += empHrs;
+}
+
+let totalEmpWage = totalEmpHrs * WAGE_PER_HRS;
+console.log("Total Employee Hours : " + totalEmpHrs + " Total Employee Wage : " + totalEmpWage);
+
 
 
 
