a6d4157f4a9c28293ed2880fe3c621be4a1b2130
megha angadi <meghamangadi@gmail.com>
2021-10-10 12:07:14 +0530

    Added: Program to Calculate Wages till a condition of total working hours of 160 or max days of 20 is reached for a month

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -5,7 +5,9 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HRS = 4;
 const FULL_TIME_HRS = 8;
 const WAGE_PER_HRS = 20;
-const NUM_OF_WORKING_DAYS = 20;
+const MAX_WORKING_DAYS = 20;
+const MAX_WORKING_HOURS = 160;
+
 
 function getWorkingHours(empcheck) {
     switch (empcheck) {
@@ -21,14 +23,16 @@ function getWorkingHours(empcheck) {
     }
 }
 let totalEmpHrs = 0;
-for (let day = 1; day <= NUM_OF_WORKING_DAYS; day++) {
+let totalWorkingDays = 0;
+while (totalEmpHrs <= MAX_WORKING_HOURS && totalWorkingDays < MAX_WORKING_DAYS) {
     let empcheck = Math.floor(Math.random() * 10) % 3;
     getWorkingHours(empcheck);
     totalEmpHrs += empHrs;
+    totalWorkingDays++;
 }
 
 let totalEmpWage = totalEmpHrs * WAGE_PER_HRS;
-console.log("Total Employee Hours : " + totalEmpHrs + " Total Employee Wage : " + totalEmpWage);
+console.log("Total Days : " + totalWorkingDays + " Total Employee Hours : " + totalEmpHrs + " Total Employee Wage : " + totalEmpWage);
 
 
 
