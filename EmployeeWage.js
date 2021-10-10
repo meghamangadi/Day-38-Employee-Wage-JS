4c4a6794eaf2344b29e597660fe60503e5a13c57
megha angadi <meghamangadi@gmail.com>
2021-10-10 11:22:16 +0530

    Added: Program to Refractor the code to write a function to get work hours

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,29 +1,31 @@
 console.log("Welcome to the Employee Wage Computation Program");
 
-const IS_PAR_TTIME=1;
-const IS_FULL_TIME=2;
-const PART_TIME_HRS=4;
-const FULL_TIME_HRS=8;
-const WAGE_PER_HRS= 20;
+const IS_PAR_TTIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HRS = 4;
+const FULL_TIME_HRS = 8;
+const WAGE_PER_HRS = 20;
 
-let empHrs=0;
+let empHrs = 0;
+let empcheck = Math.floor(Math.random() * 10) % 3;
+getWorkingHours(empcheck);
+let empWage = empHrs * WAGE_PER_HRS;
+console.log("Daily Employee Wage :" + empWage);
 
-let empcheck = Math.floor(Math.random()*10)%3;
 
-switch(empcheck){
-case IS_PAR_TTIME:
-    empHrs= PART_TIME_HRS;
-    break;
-case IS_FULL_TIME:
-    empHrs= FULL_TIME_HRS;
-    break;
-default :
-    empHrs=0;
-    
+function getWorkingHours(empcheck) {
+    switch (empcheck) {
+        case IS_PAR_TTIME:
+            empHrs = PART_TIME_HRS;
+            break;
+        case IS_FULL_TIME:
+            empHrs = FULL_TIME_HRS;
+            break;
+        default:
+            empHrs = 0;
+
+    }
 }
-let empWage = empHrs * WAGE_PER_HRS ;
-console.log("Daily Employee Wage :"+ empWage);
-
 
 
 
